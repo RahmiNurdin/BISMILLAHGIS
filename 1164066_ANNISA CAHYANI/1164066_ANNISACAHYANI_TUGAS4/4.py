01b66644de2bbf7f87c39abfd5c4445f7c7c884d
annisacahyani <37663703+annisacahyani@users.noreply.github.com>
2018-10-23 18:43:15 +0700

    Add files via upload

---
+++ 1164066_ANNISA CAHYANI/1164066_ANNISACAHYANI_TUGAS4/4.py
@@ -0,0 +1,10 @@
+import shapefile 
+w=shapefile.Writer(shapefile.POINTM) #deklarasi, dalam urung bisa dikosongkan boleh diisi sesuai type disini menggunakan shapefile.POINTM
+w.shapeType #berfungsi menjalankan code diatasnya
+w.field("kolom1","C") #membuat kolom1 dengan tipe data karakter
+w.field("kolom2","C") #membuat kolom2 dengan tipe data karakter
+w.record("ngek","satu") #isi dari kolom 1
+w.record("ngok","dua") #isi dari kolom 2
+w.point(1,1) #titik koordinat 1,1
+w.point(2,2) #titik koordinat 2,2
+w.save("soal4") #menyimpan soal 4
