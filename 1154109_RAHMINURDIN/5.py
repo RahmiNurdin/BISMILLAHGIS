7aca3824349e6629bc8f596ecbd881c95998fcf3
Rahmi Nurdin <sanggarsenisulota12@gmail.com>
2018-10-26 00:52:52 +0700

    Add files via upload

---
+++ 1154109_RAHMINURDIN/5.py
@@ -0,0 +1,8 @@
+import shapefile #untuk mengimport modul yang bernama shapefile
+w=shapefile.Writer() #deklarasi, dalam kurung bisa dikosongkan boleh diisi sesuai typee
+w.shapeType #berfungsi menjelaskan code diatasnya
+w.field("kolom1","C") #nama kolom1 dengan tipe data character
+w.field("kolom2","C") #nama kolom2 dengan tipe data character
+w.record("ngek","satu") #isi dari kolom 1 ngek, dan kolom 2 satu
+w.line(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]]) 
+w.save("soal5") #nama file
