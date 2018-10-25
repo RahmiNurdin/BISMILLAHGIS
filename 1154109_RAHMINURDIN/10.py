f94e4c233893554473c6b3b6c5e62999d64bd3b7
Rahmi Nurdin <sanggarsenisulota12@gmail.com>
2018-10-26 00:58:02 +0700

    Add files via upload

---
+++ 1154109_RAHMINURDIN/10.py
@@ -0,0 +1,15 @@
+import shapefile #mengimpor modul shapefil
+w=shapefile.Writer() #untuk membuat shapefile baru
+w.shapeType #menyeting menggunakan jenis shape apa (point,polygon)
+#membuat dbs dengan 2 field, berupa kolom
+w.field("Nama Bidang","Bidang Ke")
+w.field("Nama Bidang","Bidang Ke") #membuat dbs dengan 2 field, berupa kolom
+
+w.record("belah ketupat","satu")
+
+
+
+#membuat 5 row 
+w.poly(parts=[[[-2,1],[-1,2],[-2,3],[-3,2],[-2,1]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+
+w.save("soal10")#melakukan save dengan nama (soal10)
